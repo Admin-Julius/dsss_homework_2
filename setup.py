7327749337c14ac00ffbcb01f7ad2981681bd827
Admin-Julius <julius-glaser@gmx.de>
2023-11-07 23:01:06 +0100

    fixed package naming

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsss-homework-2',
-    version='0.1.0',
+    name='math_quiz',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[],
 )
